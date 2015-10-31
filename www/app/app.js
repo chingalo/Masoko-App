deb7f5b0bd347b2cbefb0216fbca6f94231b6572
Joseph Chingalo <profschingalo@gmail.com>
2015-10-31 13:40:15 +0300

    add some logics on http with anngular js

--- www/app/app.js
+++ www/app/app.js
@@ -1,54 +1,101 @@
-/**
- * Created by joseph on 10/30/15.
- */
-/*.controller('main',function($scope,$http){
-    $scope.data = true;
-    console.log('ok');
-    $scope.login = function(){
-        base = 'http://localhost:8080/demo';
-        var url = base + '/dhis-web-commons-security/login.action?authOnly=true';
-        // var url = base + '/api/me.json';
-        *//* $http.get(url,{
-         params : {
-         j_username: "admin",
-         j_password: "IROAD2015"
-         },'Content-Type': 'application/json'}).success(function(data){
-         console.log(data);
-         });*//*
-
-        $http.post(
-            url,{},{withCredentials: true, params : {
-                j_username: "admin",
-                j_password: "IROAD2015"
-
-            }}).then(function(data){
-                console.log(JSON.stringify(data));
-                if(! data.data){
-                    alert('success');
-                }
-                else{
-                    alert('fails');
-                }
-            },function(error){
-                alert(JSON.stringify(error))
-            });
-    }
-})*/
-
-/*
-
-$scope.login = function(){
-
-    console.log('login user : ' + JSON.stringify($scope.loginUser));
-    //try to make request to login
-    var url = baseUrl + 'rest/login';
-    $http.post(
-        url,{"email":"profschingalo@gmail.com", "password":"chingalo"},{}).then(function(data){
-            console.log(JSON.stringify(data));
-
-        },function(error){
-            console.log(JSON.stringify(error))
-        });
-
-
-}*/
+///**
+// * Created by joseph on 10/30/15.
+// */
+///*.controller('main',function($scope,$http){
+//    $scope.data = true;
+//    console.log('ok');
+//    $scope.login = function(){
+//        base = 'http://localhost:8080/demo';
+//        var url = base + '/dhis-web-commons-security/login.action?authOnly=true';
+//        // var url = base + '/api/me.json';
+//        *//* $http.get(url,{
+//         params : {
+//         j_username: "admin",
+//         j_password: "IROAD2015"
+//         },'Content-Type': 'application/json'}).success(function(data){
+//         console.log(data);
+//         });*//*
+//
+//        $http.post(
+//            url,{},{withCredentials: true, params : {
+//                j_username: "admin",
+//                j_password: "IROAD2015"
+//
+//            }}).then(function(data){
+//                console.log(JSON.stringify(data));
+//                if(! data.data){
+//                    alert('success');
+//                }
+//                else{
+//                    alert('fails');
+//                }
+//            },function(error){
+//                alert(JSON.stringify(error))
+//            });
+//    }
+//})*/
+//
+///*
+// /*
+// *Function to handle login process to dspace system
+// * Takes username as email and password from the form
+// */
+//$scope.user = {};
+//$scope.loginUser = {};
+//$scope.login = function(){
+//
+//
+//    console.log('login user : ' + JSON.stringify($scope.loginUser));
+//    //try to make request to login
+//    var url = baseUrl + 'rest/login';
+//    //{"email":"profschingalo@gmail.com", "password":"chingalo"}
+//    $http.post(url,{"email":$scope.loginUser.email, "password":$scope.loginUser.password},{})
+//        .then(function(response){
+//            console.log('success login');
+//            console.log('data : ' + response.data);
+//
+//            var url = baseUrl + 'rest/status';
+//            $http.get(url,{
+//                headers :{
+//                    'rest-dspace-token' : response.data
+//                }
+//            }).then(function(data){
+//                console.log(JSON.stringify(data));
+//                $scope.user = data.data;
+//
+//                //$scope.logOut();
+//            },function(error){
+//                console.log('fail obtain user details' + JSON.stringify(error))
+//            });
+//
+//        },function(error){
+//            console.log('fail to login');
+//            console.log(JSON.stringify(error))
+//        });
+//
+//
+//}
+//
+///*
+// *function to handle log out functionality
+// */
+//$scope.logOut = function () {
+//
+//    console.log('login user : ' + JSON.stringify($scope.user.token ));
+//    var url = baseUrl + 'rest/logout';
+//    var token = $scope.user.token;
+//    $http.post(url,{},{
+//        headers :{
+//            'rest-dspace-token' : token
+//        }
+//    }).then(function(data){
+//            console.log(JSON.stringify(data));
+//        }
+//        ,function(error){
+//            console.log('fail obtain user details' + JSON.stringify(error))
+//        }
+//    );
+//}
+//
+//
+//}*/
