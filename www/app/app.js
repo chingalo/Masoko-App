57604b94b5b4d5d115e3d80da069daea2306fb1b
Joseph Chingalo <profschingalo@gmail.com>
2015-11-09 12:17:57 +0300

    testing geo location with btn

--- www/app/app.js
+++ www/app/app.js
@@ -1,5 +1,6 @@
 angular.module('blogSample',[])
     .controller('mainController', function ($scope, $rootScope) {
+        $scope.location = false;
         var onSuccess = function(position) {
             $rootScope.$apply(function() {
                 $scope.position = position;
@@ -9,6 +10,9 @@ angular.module('blogSample',[])
         var onError = function(error) {
             alert('ERROR! code: ' + error.code + ' ' + 'message: ' + error.message);
         };
-        navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true});
 
+        $scope.getMyLocation = function(){
+            $scope.location = true;
+            navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true});
+        }
     });
