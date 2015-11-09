1488086d4a959ec04a3ea1ca6fca1b5dbd2844fb
Joseph Chingalo <profschingalo@gmail.com>
2015-11-09 10:00:34 +0300

    fix

--- www/app/app.js
+++ www/app/app.js
@@ -1,12 +1,14 @@
-angular.module('blogSample')
+angular.module('blogSample',[])
     .controller('mainController', function ($scope, $rootScope) {
         var onSuccess = function(position) {
             $rootScope.$apply(function() {
                 $scope.position = position;
+                alert(JSON.stringify(position));
             });
         };
         var onError = function(error) {
-            console.log('ERROR! code: ' + error.code + ' ' + 'message: ' + error.message);
+            alert('ERROR! code: ' + error.code + ' ' + 'message: ' + error.message);
         };
         navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true});
+
     });
