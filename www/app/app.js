03718c251181a47a6a30f1180c01c6ac3432af1c
Joseph Chingalo <profschingalo@gmail.com>
2015-11-09 09:20:14 +0300

    testing geo location

--- www/app/app.js
+++ www/app/app.js
@@ -1,3 +1,35 @@
+var blogSample = angular.module('blogSample', ['ngRoute','ui.bootstrap']);
+blogSample.controller('mainController', function($scope, $route){
+    $scope.latitude = blogSample.latitude;
+    $scope.longitude = blogSample.longitude;
+});
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        navigator.geolocation.getCurrentPosition(app.onSuccess, app.onErr);
+    },
+    onErr: function(error)
+    {
+        alert('Unable to get your location.Without location you will not be able to use navigate feature! Error:' + '\n' + error.message);
+    },
+
+    onSuccess: function(position)
+    {
+        blogSample.latitude = position.coords.latitude;
+        blogSample.longitude = position.coords.longitude;
+    }
+};
+
+
+
 ///**
 // * Created by joseph on 10/30/15.
 // */
