34e7a603729c0b3a94e14914bf66ce5ad6347bc1
Joseph Chingalo <profschingalo@gmail.com>
2015-11-20 09:06:24 +0300

    testing media capture

--- www/app/app.js
+++ www/app/app.js
@@ -15,4 +15,35 @@ angular.module('blogSample',[])
             $scope.location = true;
             navigator.geolocation.getCurrentPosition(onSuccess, onError, {timeout: 10000, enableHighAccuracy: true});
         }
+
+        // Called when capture operation is finished
+        //
+        var captureSuccess = function(mediaFiles) {
+            var i, len;
+            for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+                //uploadFile(mediaFiles[i]);
+                path = mediaFiles[i].fullPath;
+                alert(JSON.stringify(path));
+            }
+        }
+
+        // Called if something bad happens.
+        //
+        var captureError =function(error) {
+            var msg = 'An error occurred during capture: ' + error.code;
+            navigator.notification.alert(msg, null, 'Uh oh!');
+        }
+
+        $scope.takeVideo = function(){
+
+            navigator.device.capture.captureVideo(captureSuccess, captureError, {limit: 2});
+        }
+
+        $scope.takePhoto = function (){
+
+            navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 2});
+        }
+
+
+
     });
