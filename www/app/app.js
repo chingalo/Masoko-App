22baa1e114e786277640c74c97e2c8f8c890e194
Joseph Chingalo <profschingalo@gmail.com>
2015-12-10 17:04:21 +0300

    testing barcode reader

--- www/app/app.js
+++ www/app/app.js
@@ -21,10 +21,7 @@ angular.module('app',['ngStorage','ngCordova'])
 
             cordova.plugins.barcodeScanner.scan(
                 function (result) {
-                    alert("We got a barcode\n" +
-                    "Result: " + result.text + "\n" +
-                    "Format: " + result.format + "\n" +
-                    "Cancelled: " + result.cancelled);
+                    alert(result);
                 },
                 function (error) {
                     alert("Scanning failed: " + error);
