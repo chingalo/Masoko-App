6a3eedc514cc619498753d62365974ee2dd7657c
Joseph Chingalo <profschingalo@gmail.com>
2015-12-11 15:45:01 +0300

    testing signature pad

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,18 @@
 angular.module('app',['ngStorage'])
+    .controller('SignatureCtrl', function($scope) {
+        var canvas = document.getElementById('signatureCanvas');
+        var signaturePad = new SignaturePad(canvas);
+
+        $scope.clearCanvas = function() {
+            signaturePad.clear();
+        }
+
+        $scope.saveCanvas = function() {
+            var sigImg = signaturePad.toDataURL();
+            $scope.signature = sigImg;
+            window.open(signaturePad.toDataURL());
+        }
+    })
     .controller('mainController',function($scope,$localStorage){
 
         $scope.barcode = function(){
